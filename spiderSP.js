799c2cc485554a5213cd415ef89240a5d24e35c7
37485345@qq.com <37485345@qq.com>
2018-12-05 18:11:40 +0800

    增加1688

--- spiderSP.js
+++ spiderSP.js
@@ -4,10 +4,115 @@ const moment = require('moment');
 
 const curDate =moment(new Date(),"YYYYMMDDHHmmss");
 
-let productList;
-let spLists;
+let productList=[];
+let spLists=[];
 
 async function getProducts() {
+    let isEnd=false;
+    let isEndNo=1;
+    const browser = await puppeteer.launch();
+    try {
+        const page = await browser.newPage();
+        await page.goto('https://p4psearch.1688.com/p4p114/p4psearch/offer2.htm?cosite=360jj&keywords=%E4%BF%9D%E5%81%A5%E5%93%81&trackid=8856888022272688587535&location=re&province=&city=&provinceValue=%E6%89%80%E5%9C%A8%E5%9C%B0%E5%8C%BA&sortType=&descendOrder=&priceStart=&priceEnd=&dis=');
+
+        await page.waitForSelector('.next-loading-component');
+
+         while (!isEnd) {
+            // await page.waitFor(2500);
+            const tempObj = await handleProductData(page);
+
+            const isEndTemp = await tempObj['page'];
+            isEnd=await tempObj['ds'];
+            if(isEndTemp!=0){
+                await isEndNo++;
+            }
+            await console.log(isEndNo+":"+tempObj['ds']);
+            await page.waitFor(2500);
+        }
+        await console.log(productList);
+        let writerStream = fs.createWriteStream('productLinks-'+curDate+'.txt');
+        await  writerStream.write(JSON.stringify(productList, undefined, 2), 'UTF8');
+        await writerStream.end();
+        await browser.close();
+
+        for(let i=0;i<productList.length;i++){
+            const tempSP=await getSPs(productList[i]);
+            await console.log(tempSP);
+        }
+        await console.log(spLists);
+        writerStream = await fs.createWriteStream('spLists-'+curDate+'.txt');
+        await writerStream.write(JSON.stringify(spLists, undefined, 2), 'UTF8');
+        await writerStream.end();
+    }catch (error) {
+        // 出现任何错误，打印错误消息并且关闭浏览器
+        console.log(error)
+        console.log('服务意外终止');
+        await browser.close()
+    } finally {
+        // 最后要退出进程
+        process.exit(0)
+    }
+}
+
+async function handleProductData(page) {
+    const list = await page.evaluate(( )=> {
+        let productLinks=[];
+        let productObj=new Object();
+        const productsDiv =document.querySelectorAll('.next-loading .offer_item');
+        productsDiv.forEach(function(pdiv){
+            //获取商品的详细链接
+            let links = pdiv.querySelector(".img a").getAttribute("href");
+            if(links.indexOf('https://')==-1){
+                links="https:"+links;
+            }
+            productLinks.push(links);
+        });
+        productObj['obj']=productLinks;
+
+        // const currentPage =document.querySelector('.next-pagination-list button');
+        // let isEnd=0;
+        // if(currentPage) {
+        //     productLinks.push(currentPage.innerText);
+        //     let nextPage=currentPage.nextElementSibling;
+        //     //whether have next page
+        //     if(nextPage) {
+        //         //has next page
+        //         isEnd=nextPage.innerText;
+        //         nextPage.click();
+        //     }else{
+        //         //ending, set is end to 1;
+        //         isEnd=0;
+        //     }
+        // }else{
+        //     isEnd=0;
+        // }
+
+        //get next page button
+        const nextPage =document.querySelector('button.next-btn.next-btn-normal.next-btn-large.next-pagination-item.next');
+        let isEnd=1;
+        //if next page button is disabled, that end
+        if(nextPage) {
+            console.log(nextPage.innerText);
+            const ds=nextPage.disabled;
+            if(!ds){
+                nextPage.click();
+            }else{
+                isEnd=0;
+            }
+            productObj['ds']=ds;
+        }else{
+            isEnd=0;
+        }
+
+        productObj['page']=isEnd;
+        return productObj;
+    });
+    await page.waitFor(4500);
+    productList=productList.concat(list['obj']);
+    return list;
+}
+
+async function getProductsT() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://p4psearch.1688.com/p4p114/p4psearch/offer2.htm?cosite=360jj&keywords=%E4%BF%9D%E5%81%A5%E5%93%81&trackid=8856888022272688587535&location=re&province=&city=&provinceValue=%E6%89%80%E5%9C%A8%E5%9C%B0%E5%8C%BA&sortType=&descendOrder=&priceStart=&priceEnd=&dis=');
@@ -45,7 +150,7 @@ async function getProducts() {
     });
 
     console.log(productList);
-    let writerStream = fs.createWriteStream('productLinks-'+curDate+'.txt');
+    let writerStream =await fs.createWriteStream('productLinks-'+curDate+'.txt');
     writerStream.write(JSON.stringify(productList, undefined, 2), 'UTF8');
     writerStream.end();
     await browser.close();
@@ -64,16 +169,17 @@ async function getProducts() {
 async function getSPs(index) {
 
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(index);
 
-    // await page.waitFor(6000);
-    // await page.waitForSelector('div .mod-contactSmall', { timeout: 100000, visible: true });
     try{
-        const spList = await page.evaluate(( )=> {
-            let spList=new Object();
+        const page = await browser.newPage();
+        await page.goto(index);
+
+        await page.waitFor(6000);
+        // await page.waitForSelector('div.mod-contactSmall', { timeout: 100000, visible: true });
+        const spObj = await page.evaluate(( )=> {
             let sp=new Object();
             let spDiv =document.querySelector('#site_content .mod-contactSmall');
+            if(!spDiv) return "load unready";
             let nameDiv=spDiv.querySelector(".membername");
             let name;
             if(nameDiv){
@@ -106,23 +212,28 @@ async function getSPs(index) {
             let dataView= JSON.parse(spDiv.getAttribute("data-view-config"));
             sp["company"]=dataView.companyName;
             sp["address"]=dataView.address;
-            spList[sp["phone"]]=sp;
-            return spList;
+            return sp;
         });
+        spLists =spLists.push(spObj);
+        await console.log(spObj);
+        // let writerStream = fs.createWriteStream('spList-'+curDate+'.txt');
+        // writerStream.write(JSON.stringify(spList, undefined, 2), 'UTF8');
+        // writerStream.end();
+        await browser.close();
 
-        return spList;
-        console.log(spList);
+        return spObj;
     }catch(e){
         console.log(e);
+        console.log('服务意外终止');
+        await browser.close();
+    } finally {
+        // 最后要退出进程
+        process.exit(0)
     }
 
-    // let writerStream = fs.createWriteStream('spList-'+curDate+'.txt');
-    // writerStream.write(JSON.stringify(spList, undefined, 2), 'UTF8');
-    // writerStream.end();
-    await browser.close();
 
 
 }
 
-getProducts();
-// getSPs("https://dj.1688.com/ci_bb?a=2004000869&e=MRrS4nBmwy7bK9PN7bbqDCK13sgXILWW8YnxVi7mVjCwKEHkNmvLVO2OCijk72T8wCcBssQTpt7AEFoxEOKKf0kmEeFvaj-eLfKnp1GFDxcPHcJGU3q9O9N-0MlRF58FahkfjKENNWwVXwOLilNlV-aWRi0eQKYOJCARaNkKQYveuvPhGrEsYHsB-XTovd5ex03jE8.HEM7SqGb.xd2xe-OkEc8Rzh60dXyKs0scpqSB-VmuN27UYg8gU6Drr-ZbNGq5vHY6xPASAxG6leuuovltMGo1-ZGvtBBqQKeuNN5x37cZTwHLJSLxtxhqfLLkcgEEkfVUM6htJf1w5lOe.ieOair6XedsAxuFZgCWWxfbJmrP.hfijbI6MOwpZrakLg-iKpw2JLqLa6Z426nr79dDr7HXIt8vaz9g2v1sQv-OCiQwXTRMbf4s4nec4mK0wDkExP.aW3NL6nZtAfrqwpu2k9a8lVgJWc4gnppTSmEXDgKiSwGsVdJSdnVvOB6IOwE5LQsWF3vuZu1lG2-Y0eptRXo7fj0bZ.NfS6jaDUDQNG33CYNCJy0tu5ty0fk-dyrjxoEYFk29orR-UySYehKj2kf9WyAeyAwfJVUnMj8frVG4a3psppHVsMCMVpOydmijhWAB8ckIwMp7hXw9kA__&v=4&ap=2&rp=2");
+// getProducts();
+getSPs("https://detail.1688.com/offer/552929152950.html");
