5073882a56d17b15f05e35225dd032f44ad7e00e
37485345@qq.com <37485345@qq.com>
2018-12-06 14:56:44 +0800

    增加1688

--- spiderSP.js
+++ spiderSP.js
@@ -1,8 +1,11 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const util = require('util')
 const moment = require('moment');
+const json2csv = require('json2csv');
 
 const curDate =moment(new Date(),"YYYYMMDDHHmmss");
+const fs_writeFile = util.promisify(fs.writeFile);
 
 let productList=[];
 let spLists=[];
@@ -13,11 +16,16 @@ async function getProducts() {
     const browser = await puppeteer.launch();
     try {
         const page = await browser.newPage();
+        //china
         await page.goto('https://p4psearch.1688.com/p4p114/p4psearch/offer2.htm?cosite=360jj&keywords=%E4%BF%9D%E5%81%A5%E5%93%81&trackid=8856888022272688587535&location=re&province=&city=&provinceValue=%E6%89%80%E5%9C%A8%E5%9C%B0%E5%8C%BA&sortType=&descendOrder=&priceStart=&priceEnd=&dis=');
+        //fujian
+        //await page.goto('https://p4psearch.1688.com/p4p114/p4psearch/offer2.htm?cosite=360jj&keywords=%E4%BF%9D%E5%81%A5%E5%93%81&trackid=8856888022272688587535&location=re&province=&city=&provinceValue=%E7%A6%8F%E5%BB%BA&sortType=&descendOrder=&priceStart=&priceEnd=&dis=%27');
+        //xiamen
+        //await page.goto('https://p4psearch.1688.com/p4p114/p4psearch/offer2.htm?cosite=360jj&keywords=%E4%BF%9D%E5%81%A5%E5%93%81&trackid=8856888022272688587535&location=re&province=%E7%A6%8F%E5%BB%BA&city=%E5%8E%A6%E9%97%A8&provinceValue=%E5%8E%A6%E9%97%A8&sortType=&descendOrder=&priceStart=&priceEnd=&dis=%27');
 
         await page.waitForSelector('.next-loading-component');
 
-         while (!isEnd) {
+         while (await !isEnd) {
             // await page.waitFor(2500);
             const tempObj = await handleProductData(page);
 
@@ -30,19 +38,38 @@ async function getProducts() {
             await page.waitFor(2500);
         }
         await console.log(productList);
-        let writerStream = fs.createWriteStream('productLinks-'+curDate+'.txt');
+        let writerStream = await fs.createWriteStream('productLinks-'+curDate+'.txt');
         await  writerStream.write(JSON.stringify(productList, undefined, 2), 'UTF8');
         await writerStream.end();
+
+        // const productFields = ['link'];
+        // const productCsv = await json2csv.parse(productList, { fields: productFields});
+        // await fs.writeFile('productFile-'+curDate+'.csv', productCsv, function(err) {
+        //     if (err) throw err;
+        //     console.log('file saved');
+        // });
         await browser.close();
 
         for(let i=0;i<productList.length;i++){
             const tempSP=await getSPs(productList[i]);
-            await console.log(tempSP);
+            // await console.log(tempSP);
         }
         await console.log(spLists);
-        writerStream = await fs.createWriteStream('spLists-'+curDate+'.txt');
-        await writerStream.write(JSON.stringify(spLists, undefined, 2), 'UTF8');
-        await writerStream.end();
+        const spFields = ['name','tele','phone','fax','company','address'];
+        const spCsv = await json2csv.parse(spLists, { fields: spFields});
+        await console.log(spCsv);
+        await fs_writeFile('spFile-'+curDate+'.csv', spCsv)
+            .catch((error) => {
+                console.log(error)
+            });
+        // await fsPromises.writeFile('spFile-'+curDate+'.csv', spCsv, function(err) {
+        //     if (err)  console.log(err);
+        //     console.log('file saved');
+        // });
+        //
+        // let writerStream2 = await fs.createWriteStream('spLists-'+curDate+'.txt');
+        // await writerStream2.write(JSON.stringify(spLists, undefined, 2), 'UTF8');
+        // await writerStream2.end();
     }catch (error) {
         // 出现任何错误，打印错误消息并且关闭浏览器
         console.log(error)
@@ -92,7 +119,7 @@ async function handleProductData(page) {
         let isEnd=1;
         //if next page button is disabled, that end
         if(nextPage) {
-            console.log(nextPage.innerText);
+            // console.log(nextPage.innerText);
             const ds=nextPage.disabled;
             if(!ds){
                 nextPage.click();
@@ -172,14 +199,19 @@ async function getSPs(index) {
 
     try{
         const page = await browser.newPage();
+        // await page.goto(index,{ timeout: 100000 });
         await page.goto(index);
 
-        await page.waitFor(6000);
-        // await page.waitForSelector('div.mod-contactSmall', { timeout: 100000, visible: true });
+        await page.waitFor(10000);
+        // await page.waitForSelector('#site_content.mod-contactSmall', { timeout: 100000, visible: true });
+        await page.screenshot({path: 'example.png',fullPage:true});
         const spObj = await page.evaluate(( )=> {
             let sp=new Object();
             let spDiv =document.querySelector('#site_content .mod-contactSmall');
-            if(!spDiv) return "load unready";
+            if(!spDiv) {
+                sp["name"]="no data";
+                return sp;
+            };
             let nameDiv=spDiv.querySelector(".membername");
             let name;
             if(nameDiv){
@@ -214,11 +246,19 @@ async function getSPs(index) {
             sp["address"]=dataView.address;
             return sp;
         });
-        spLists =spLists.push(spObj);
-        await console.log(spObj);
+        await page.waitFor(2500);
+        spLists.push(spObj);
+        await console.log(spLists);
         // let writerStream = fs.createWriteStream('spList-'+curDate+'.txt');
         // writerStream.write(JSON.stringify(spList, undefined, 2), 'UTF8');
         // writerStream.end();
+        const spFields = ['name','tele','phone','fax','company','address'];
+        const spCsv = await json2csv.parse(spLists, { fields: spFields});
+        // await console.log(spCsv);
+        await fs_writeFile('spFile-'+curDate+'.csv', spCsv)
+            .catch((error) => {
+                console.log(error)
+            });
         await browser.close();
 
         return spObj;
@@ -227,8 +267,7 @@ async function getSPs(index) {
         console.log('服务意外终止');
         await browser.close();
     } finally {
-        // 最后要退出进程
-        process.exit(0)
+
     }
 
 
@@ -236,4 +275,4 @@ async function getSPs(index) {
 }
 
 // getProducts();
-getSPs("https://detail.1688.com/offer/552929152950.html");
+getSPs("https://dj.1688.com/ci_bb?a=282324907&e=bXr3J13ombDbJh1XfjDyDQFF2yxP1boBh5CIXbq9YT2c8GcaPy-9JnTiA4FO8mnbGJN-fd.xeOHJiRcoQLkAYPTNbKJ4zC73CFIAf7DoiJZakLbEG8DwZYNA42.97W2t9u7Gz07jruWYeTtrioq9-0.viMonhcuuMOo9Q-0KIuPsYerPJJVJ.74dODWPMcA9iZzg8TrynUfv7UQKHlvbGiBgTkxjc6f6P.34lVOLFeZI47E3-jmO43qaSUOq314RVFAylNtGQ1nfj4luE5r-3ViCzgayBqC517P5OBDj4i5wUKgLd3T.MtK-3EfiBHvH5r67ImQPffXPD8ZS5HPyNHdsaUdBGuvwnFUmplH-zEgwWtiY3Bw6YM3H1-n2O1L5LYsQNMwhmo4VoyugT9ZVcnqTVyYxW2Vd63VgREbhT.2Sr.FcWqPKLNP.OxwpUlLlVvI07c0HgPu9QtK7JdZQWRljue0XWRg.EJ33VEGIe3inix-BSUD0PYr.aTJv0Uu5pTZyAYBu8C6JEcbQuovqMn2T8RqJJ0QWnfQft2gNnVsG8-FBa4GYxpIaECvRwUBtcbPYcrBJC6gLi8XriVXiBg__&v=4&ap=176&rp=16");
--- test.js
+++ test.js
@@ -1,3 +1,6 @@
+const json2csv = require('json2csv');
+const fs = require('fs');
+
 let tt="新疆：\\t";
 
 console.log(tt.replace(/：\\t/ig,""))
@@ -7,4 +10,27 @@ const brands=[];
 chars.forEach(function (cs) {
     const pageId = "#html" + cs;
     console.log(pageId)
+});
+
+const fields =  ['name','tele','phone','fax','company','address'];
+const myCars = [ { name: 'load unready' },
+    { name: 'load unready' },
+    { name: 'load unready' } ];
+;
+
+let writerStream = fs.createWriteStream('productLinks-'+'.txt');
+writerStream.write(JSON.stringify(myCars, undefined, 2), 'UTF8');
+writerStream.end();
+
+const csv = json2csv.parse(myCars, { fields: fields});
+fs.writeFile('file.csv', csv, function(err) {
+    if (err) throw err;
+    console.log('file saved');
+});
+
+
+const csv2 = json2csv.parse(myCars, { fields: fields});
+fs.writeFile('file1.csv', csv2, function(err) {
+    if (err) throw err;
+    console.log('file saved');
 });
